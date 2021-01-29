7182da4634719802e6ddbc0206ad5032bac85678
HHBB0066 <78001111+HHBB0066@users.noreply.github.com>
2021-01-29 09:08:24 -0300

    Update defss.py

--- defss.py
+++ defss.py
@@ -6,12 +6,15 @@ def add(x):
 	DB.DB.append(x)
 	print('Added')
 def acss():
-	x = int(input('num for DB: '))
-	print(DB.DB[x])
-	if x not in DB.DB:
+	x = int(input('num for DB '))
+	if DB.DB[x] not in DB.DB:
 		print("sorry this number isn't in DB")
+	elif DB.DB[x] in DB.DB:
+		print((DB.DB[x]))
 def remove():
         x = int(input('num for remove in DB: '))
         rmv = DB.DB[x]
         DB.DB.remove(rmv)
         print('removed')
+       
+        
